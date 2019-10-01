b3fdcc4010d35ebffe260cb3367ca5a85f2c6b51
moudev <rigobertomauriciomartinez@gmail.com>
2019-10-01 17:22:40 -0600

    Add environment variables into the project
      - Adding .env file
      - Correction of indentation in src/Map.js

--- src/Map.js
+++ src/Map.js
@@ -2,90 +2,81 @@ import React, {Component} from 'react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 const Map = ReactMapboxGl({
-  accessToken: "pk.eyJ1IjoiY2hyaXNmdDI1IiwiYSI6ImNqemxheXBpaTB0bXczY2s2MG5rM3BrZzgifQ.IJuNpj8FMzhcaHNXSeJM8w",
+  accessToken: process.env.REACT_APP_MAP_KEY,
 });
 
 class Mapa extends Component {
-
   state={
-    lat:null,
-    long:null,
+    lat: null,
+    long: null,
     counter: 0,
-    center:[]
+    center: []
   }
 
-constructor(props){
-  super(props);
-  
-}
-
-componentDidMount(){
-
-  this.interval2 = setInterval(()=>{
-    this.setState({
-      counter: this.state.counter+1
-    })
-  },1200);
-
-  this.interval = setInterval(() => {
-    fetch('http://67.205.188.226:3000/coordenadas')
-    .then((data)=>{
-      return data.json()
-    }).then((data =>{
-
+  componentDidMount() {
+    this.interval2 = setInterval(()=>{
       this.setState({
-        lat:data.data.latitud,
-        long:data.data.longitud,
+        counter: this.state.counter+1
       })
-    }))
-    
-  }, 1000);
+    },1200);
+
+    this.interval = setInterval(() => {
+      fetch(process.env.REACT_APP_API_URL)
+        .then((data)=>{
+          return data.json()
+        }).then((data =>{
+
+          this.setState({
+            lat:data.data.latitud,
+            long:data.data.longitud,
+          })
+        }))
+
+    }, 1000);
   }
 
-componentWillUnmount() {
-  clearInterval(this.interval);
-  clearInterval(this.interval2);
-}
-
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    clearInterval(this.interval2);
+  }
 
   render(){
-
-  return (
-    <div className="App" style={{textAlign:"center"}}>
-      {
-        
-        (this.state.lat) ? (
-          <div>
-            <h1 style={{position:"relative",zIndex:"2"}}>BASTÓN INTELIGENTE</h1>
-          <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            zoom={[16]}
-            center={[this.state.long,this.state.lat]}
-            containerStyle={{
-              height: "90vh",
-              width: "90vw",
-              position: "absolute",
-              top: "55%",
-              left: "50%",
-              transform: "translate(-50%,-50%)",
-              border: "20px solid red",
-              marginBottom:"10px"
-            }}
-            renderChildrenInPortal={true}>
+    return (
+      <div className="App" style={{textAlign:"center"}}>
+        {
+          (this.state.lat) ? (
+            <div>
+              <h1 style={{position:"relative",zIndex:"2"}}>BASTÓN INTELIGENTE</h1>
+              <Map
+                 style="mapbox://styles/mapbox/streets-v9"
+                 zoom={[16]}
+                 center={[this.state.long,this.state.lat]}
+                 containerStyle={{
+                   height: "90vh",
+                   width: "90vw",
+                   position: "absolute",
+                   top: "55%",
+                   left: "50%",
+                   transform: "translate(-50%,-50%)",
+                   border: "20px solid red",
+                   marginBottom:"10px"
+                 }}
+                 renderChildrenInPortal={true}
+              >
                 <Layer
-                  type="symbol"
-                  id="marker"
-                  layout={{ "icon-image": "marker-15", "icon-size":2 }}
-                  key={this.state.counter}>
+                   type="symbol"
+                   id="marker"
+                   layout={{ "icon-image": "marker-15", "icon-size":2 }}
+                   key={this.state.counter}>
                   <Feature coordinates={[this.state.long,this.state.lat]}/>
                 </Layer>
-            </Map>  
-                  </div>
-        ) : 'Cargando data'
-      }
-    </div>
-  );
-}
+              </Map>
+            </div>
+          ) : 'Cargando data'
+        }
+      </div>
+    );
+  }
 }
 
-export default Mapa
+export default Mapa;
